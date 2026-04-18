a6e6a78f29ff8e8ed9f37ec6db8ce8fc95d6908a
Xmerlin7 <seifahmedjr7@gmail.com>
2026-04-18 14:33:13 +0200

    Refactor Post model to use creator association and add PostEditor model for managing post editors

--- db/migrate/20260418112559_create_posts.rb
+++ db/migrate/20260418112559_create_posts.rb
@@ -1,10 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[8.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :content
-      t.references :user, null: false, foreign_key: true
-
+      t.string :title, null: false
+      t.text :content, null: false
+      t.references :creator, null: false, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
